69aa2be973a44a84182de209d8e26049554426d8
coil <75682926+ilsung12@users.noreply.github.com>
2021-01-19 13:48:40 +0900

    Update 210118.py

--- 210118.py
+++ 210118.py
@@ -6,9 +6,51 @@
 # .. : 부모디렉토리
 # .  : 현재디렉토리
 
- # 사용 1 (클래스 형태)
+
+
+
+ # Fibonacci
+# class Fibonacci:
+#  def __init__(self, title="fibonacci"):
+#   self.title = title
+  
+#  def fib(n):
+#   a, b = 0, 1
+#   while a < n :
+#    print(a, end = '')
+#    a, b = b, a + b
+#   print()
+  
+#  def fib2(n):
+#   result = []
+#   a, b = 0, 1
+#   while a < n :
+#    result.append(a)
+#    a, b = b, a+b
+#   return result
+  
+ 
+ 
+ # calculation
+# def add (l, r):
+#  return l + r
+
+
+# def mul (l, r):
+#  return l * r
+
+
+# def div (l, r):
+#  return l / r
+  
+      
+
+
+
+ # 사용 1 (Fibo -> 클래스 형태 -> from y.x import x)
 
 import builtins
+
 from pkg.fibonacci import Fibonacci
 
 Fibonacci.fib(300)
@@ -17,6 +59,8 @@ print("ex1: ", Fibonacci.fib2(400))
 print("ex1: ", Fibonacci().title)
 
 
+print("-------------------------------------------------------------------")
+
 # 사용 2 (클래스 형태, 권장x)
 from pkg.fibonacci import *
 
@@ -24,6 +68,9 @@ Fibonacci.fib(500)
 
 print("ex2: ", Fibonacci.fib2(600))
 print("ex2: ", Fibonacci().title)
+
+
+print("-------------------------------------------------------------------")
 
 
 # 사용 3 (클래스 형태, alias)
@@ -35,6 +82,9 @@ print("ex3: ", fb.fib2(1600))
 print("ex3: ", fb().title)
 
 
+print("-------------------------------------------------------------------")
+
+
 # 사용 4 (함수 형태)
 import pkg.calculation as c
 
@@ -42,9 +92,15 @@ print("ex4 : ", c.add(10, 100))
 print("ex4 : ", c.mul(10, 100))
 
 
+print("-------------------------------------------------------------------")
+
+
 # 사용 5 (함수)
 from pkg.calculation import div as d
 print("ex5 : ", int(d(100, 10)))
+
+
+print("-------------------------------------------------------------------")
 
 
 # 사용 6 
